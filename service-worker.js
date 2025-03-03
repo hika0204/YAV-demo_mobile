9cc61f98a211e00e9cb971f3fd34eafff41f6316
hika0204 <1512hikarukishi@gmail.com>
2025-03-03 09:13:54 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,14 @@
 self.addEventListener("install", (event) => {
-    console.log("Service Worker installing...");
-    event.waitUntil(
-        caches.open("pwa-test-cache").then((cache) => {
-            return cache.addAll(["index.html", "manifest.json"]);
-        })
-    );
-});
-
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  console.log("Service Worker installing...");
+  event.waitUntil(
+    caches.open("pwa-test-cache").then((cache) => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "service-worker.js",
+        "icons/icon-192x192.png",
+        "icons/icon-512x512.png"
+      ]);
+    })
+  );
 });
