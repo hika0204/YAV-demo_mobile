4057825124595e75241a1208b3398160100fcf8c
hika0204 <1512hikarukishi@gmail.com>
2025-03-03 09:21:28 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,11 +3,11 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("pwa-test-cache").then((cache) => {
       return cache.addAll([
-        "index.html",
-        "manifest.json",
-        "service-worker.js",
-        "icons/icon-192x192.png",
-        "icons/icon-512x512.png"
+        "/YAV-demo_mobile/index.html",
+        "/YAV-demo_mobile/manifest.json",
+        "/YAV-demo_mobile/service-worker.js",
+        "/YAV-demo_mobile/icons/icon-192x192.png",
+        "/YAV-demo_mobile/icons/icon-512x512.png"
       ]);
     })
   );
